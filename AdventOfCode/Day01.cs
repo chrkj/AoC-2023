ce3d12fcb8dfb8166fb13fb8c8237fffd277427b
Christian <c-kj@mail.dk>
2023-12-01 15:10:08 +0100

    Day01

---
+++ AdventOfCode/Day01.cs
@@ -0,0 +1,56 @@
+﻿using AoCHelper;
+
+namespace AdventOfCode;
+
+public class Day01 : BaseDay
+{
+    private readonly string[] _input;
+    private readonly string _numbers = "1234567890";
+    private readonly Dictionary<string,string> _replacements = new() {
+        {"one", "o1e"},
+        {"two", "t2o"},
+        {"three", "t3e"},
+        {"four", "f4r"},
+        {"five", "f5e"},
+        {"six", "s6x"},
+        {"seven", "s7n"},
+        {"eight", "e8t"},
+        {"nine", "n9e"}
+    };
+
+    public Day01()
+    {
+        string inputString = File.ReadAllText(InputFilePath);
+        _input = inputString.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        int sum = 0;
+        foreach (string line in _input)
+        {
+            string number = "";
+            number += line.FirstOrDefault(c => _numbers.Contains(c));
+            number += line.LastOrDefault(c => _numbers.Contains(c));
+            sum += Convert.ToInt32(number);
+        }
+        return new ValueTask<string>(sum.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        int sum = 0;
+        foreach (string line in _input)
+        {
+            string fixedLine = line;
+            foreach (KeyValuePair<string, string> replacement in _replacements)
+                fixedLine = fixedLine.Replace(replacement.Key, replacement.Value);
+
+            string number = "";
+            number += fixedLine.FirstOrDefault(c => _numbers.Contains(c));
+            number += fixedLine.LastOrDefault(c => _numbers.Contains(c));
+            sum += Convert.ToInt32(number);
+        }
+        return new ValueTask<string>(sum.ToString());
+    }
+}
---
+++ AdventOfCode/Day02.cs
@@ -0,0 +1,24 @@
+using AoCHelper;
+
+namespace AdventOfCode;
+
+public class Day02 : BaseDay
+{
+    private readonly string[] _input;
+
+    public Day02()
+    {
+        string _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new ValueTask<string>("1");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        
+        return new ValueTask<string>("1");
+    }
+}
