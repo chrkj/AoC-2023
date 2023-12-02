1ac997528ac70cc8c4573bd90167e5cbc35c8a7e
Christian <c-kj@mail.dk>
2023-12-02 14:26:36 +0100

    Added utils class

---
+++ AdventOfCode/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    public static class Utils
+    {
+        public static int FindFirstNumericSequence(string input)
+        {
+            Match match = Regex.Match(input, @"\d+");
+
+            if (match.Success)
+                return Convert.ToInt32(match.Value);
+            throw new InvalidOperationException("No numeric sequence found.");
+        }
+    }
+}
